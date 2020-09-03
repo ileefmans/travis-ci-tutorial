3f4d0c933035993a59f277b5f64d7cf545efee18
Ian Leefmans <iel230@nyu.edu>
2020-09-03 18:25:06 -0400

    broke build

--- test_calc.py
+++ test_calc.py
@@ -4,22 +4,22 @@ from calc import Calc
 class Test(unittest.TestCase):
     
     
-    def case1(self):
+    def test_add(self):
         result = Calc(1,2).add()
         self.assertEqual(3, result)
     
-    def case2(self):
+    def test_subtract(self):
         result = Calc(1,2).subtract()
         self.assertEqual(-1, result)
     
-    def case3(self):
+    def test_multiply(self):
         result = Calc(1,2).multiply()
         self.assertEqual(2, result)
     
-    def case4(self):
-        result = Calc(1,0).divide()
-        self.assertEqual(0, result)
+    def test_divide(self):
+        result = Calc(1,1).divide()
+        self.assertEqual(1, result)
 
 
-#if __name__ == '__main__':
-#unittest.main()
+if __name__ == '__main__':
+    unittest.main()
