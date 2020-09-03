997f91b60075a7d877d8298d9751e6f7e15ab0a8
Ian Leefmans <iel230@nyu.edu>
2020-09-03 16:45:48 -0400

    added scripts for CI test with travis

---
+++ test.py
@@ -0,0 +1,24 @@
+import unittest
+
+class Test(unittest.TestCase):
+    
+    
+    def case1(self):
+        result = Calc(1,2).add()
+        self.assertEqual(result,3)
+    
+    def case2(self):
+        result = Calc(1,2).subtract()
+        self.assertEqual(result, -1)
+    
+    def case3(self):
+        result = Calc(1,2).multiply()
+        self.assertEqual(result,2)
+    
+    def case4(self):
+        result = Calc(1,0).divide()
+        self.assertEqual(result,0)
+
+
+if __name__ == '__main__':
+    unittest.main()
